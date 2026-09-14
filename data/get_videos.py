617032840a7483acae90733d04e798b28cb20b5c
RihabMhd <rihabmahdi19@gmail.com>
2026-09-14 16:51:08 +0100

    feat:added pagination

--- data/get_videos.py
+++ data/get_videos.py
@@ -4,22 +4,49 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_KEY=os.getenv("API_KEY")
-PLAYLIST_ID="UUfcQPMZg4tNk6tKUTHbAr0g"
+API_KEY = os.getenv("API_KEY")
+PLAYLIST_ID = os.getenv("PLAYLIST_ID")
 
-url="https://www.googleapis.com/youtube/v3/playlistItems"
+url = "https://www.googleapis.com/youtube/v3/playlistItems"
 
-params={
-    "part": "snippet",
-    "playlistId": PLAYLIST_ID,
-    "maxResults": 50,
-    "key": API_KEY
-}
+videos = []
 
-response=requests.get(url, params=params)
+next_page_token = None
 
-print("status:",response.status_code)
+while True:
 
-data=response.json()
+    params = {
+        "part": "snippet",
+        "playlistId": PLAYLIST_ID,
+        "maxResults": 50,
+        "key": API_KEY
+    }
 
-print(data)
+    if next_page_token:
+        params["pageToken"] = next_page_token
+
+    response = requests.get(url, params=params)
+
+    print("status:", response.status_code)
+
+    data = response.json()
+
+    for item in data.get("items", []):
+
+        video = {
+            "video_id": item["snippet"]["resourceId"]["videoId"],
+            "title": item["snippet"]["title"],
+            "published_at": item["snippet"]["publishedAt"]
+        }
+
+        videos.append(video)
+
+    next_page_token = data.get("nextPageToken")
+
+    if not next_page_token:
+        break
+
+print("Total videos:", len(videos))
+
+for video in videos:
+    print(video)
