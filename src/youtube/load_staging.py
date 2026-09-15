55ebc95756da9f2da449c8e14b655f6b8ccc4674
RihabMhd <rihabmahdi19@gmail.com>
2026-09-15 16:42:32 +0100

    feat: load YouTube data into staging

---
+++ src/youtube/load_staging.py
@@ -0,0 +1,72 @@
+import os
+import json
+import psycopg2
+from dotenv import load_dotenv
+
+load_dotenv()
+
+DB_HOST = DB_HOST = "localhost"
+DB_PORT = os.getenv("POSTGRES_CONN_PORT")
+DB_NAME = os.getenv("ELT_DATABASE_NAME")
+DB_USER = os.getenv("ELT_DATABASE_USERNAME")
+DB_PASSWORD = os.getenv("ELT_DATABASE_PASSWORD")
+
+JSON_FILE = "data/YT_data_2026-09-15.json"
+
+
+with open(JSON_FILE,'r',encoding='utf-8') as file:
+    videos=json.load(file)
+
+print("Videos loaded from JSON:", len(videos))
+
+connection=psycopg2.connect(
+    host=DB_HOST,
+    port=DB_PORT,
+    database=DB_NAME,
+    user=DB_USER,
+    password=DB_PASSWORD
+)
+
+cursor=connection.cursor()
+
+sql = """
+insert into staging.videos (
+    video_id,
+    title,
+    published_at,
+    duration,
+    view_count,
+    like_count,
+    comment_count
+)
+values (%s, %s, %s, %s, %s, %s, %s)
+on conflict (video_id)
+do update set
+    title = EXCLUDED.title,
+    published_at = EXCLUDED.published_at,
+    duration = EXCLUDED.duration,
+    view_count = EXCLUDED.view_count,
+    like_count = EXCLUDED.like_count,
+    comment_count = EXCLUDED.comment_count;
+"""
+
+for video in videos:
+    cursor.execute(
+        sql,
+        (
+            video["video_id"],
+            video["title"],
+            video["published_at"],
+            video["duration"],
+            video["view_count"],
+            video["like_count"],
+            video["comment_count"]
+        )
+    )
+
+connection.commit()
+
+cursor.close()
+connection.close()
+
+print("Staging table updated successfully.")
